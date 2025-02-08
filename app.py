9f6968004edad661620c9317fcc65157e2fa574b
Shashank-VB <shashank.bolbandi@gmail.com>
2025-02-08 17:36:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -199,7 +199,7 @@ st.write("PSV Lane 3:", result3)
 
 # Create a DataFrame with results
 results_data = {
-    'Link Section': [link_section]  # Add Link Section to the results
+    'Link Section': [link_section] , # Add Link Section to the results,
     'AADT_HGVS': [AADT_HGVS],
     'Design Period': [design_period],
     'Total Projected AADT HGVs': [total_projected_aadt_hgvs],
